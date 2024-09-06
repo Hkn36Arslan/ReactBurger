69cf3dbe83c8064fa04b1517d1612b394cc75da2
Hakan Arslan <128188577+Hkn36Arslan@users.noreply.github.com>
2024-09-06 14:07:49 +0300

    Hakkımızda sayfası düzenlendi.

--- ReactBurger/Burger/src/Components/About/About.jsx
+++ ReactBurger/Burger/src/Components/About/About.jsx
@@ -1,8 +1,31 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import hakkimizdaImage from "../../assets/images/bunner.jpg";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      {/* Üst kısım: Resim */}
+      <div className="container-fluid p-0">
+        <img
+          src={hakkimizdaImage}
+          alt="Hakkımızda"
+          className="img-fluid w-100"
+          style={{ objectFit: "cover" }}
+        />
+      </div>
 
-export default About
+      {/* Başlık ve Paragraf */}
+      <div className="container mt-3 text-center">
+        <h1 className="mb-3">Hakkımızda</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+          quis sem at nibh elementum imperdiet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+          quis sem at nibh elementum imperdiet.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
